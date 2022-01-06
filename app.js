f724d67fcd6cf3b2fece4ff4daecad5c924a282d
Pham Duy Luan <devilstorm.luan@gmail.com>
2022-01-06 21:15:39 +0700

    Chỉnh sửa trang tạo phòng

--- app.js
+++ app.js
@@ -11,15 +11,87 @@ const io = new Server(server, {
     }
 });
 
+//Kết nối CSDL
+var db = require('mysql') // Khởi tạo CSDL
+
+var dbConnection = db.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'hexachess'
+})
+
+//Kết nối mysql
+dbConnection.connect(function(err){
+    if(err) throw err;
+
+    console.log("Đã kết nối với DTB");
+})
+
 io.on('connection', (socket) =>{
     console.log("Có user kết nối");
 
+    //Chức năng Chat
     //Server nhận dữ liệu từ client khi người dùng nhập dữ liệu và gửi
     socket.on('socketClientSendDataToServer', function(data){
         console.log("dữ liệu nhận được: " +  data);
         socket.broadcast.emit('socketServerSendDataToClient', data)
     })
 
+    //Chức năng tạo phòng
+    socket.on("socketClientCreateNewRoom", function(data){
+        console.log("Client gửi dữ liệu tạo phòng với chủ phòng có tên :" + data); 
+
+        //Tạo tên phòng ngẫu nhiên từ 1000 -> 9999
+        var vNumberRoom = Math.floor(Math.random()*(9999-1000+1)+1000);
+        var vRoomName = "Room" + vNumberRoom;
+        console.log("Tên vừa được tạo: "+ vRoomName);
+
+        //Thực hiện câu truy vấn
+        var vqueryCheckRoomNameExists = "SELECT * FROM `room` WHERE roomname = '"+vRoomName+"'"; // Tạo truy vấn kiểm tra có dữ liệu trả về với tên đó không
+
+        dbConnection.query(vqueryCheckRoomNameExists, function(err, result){ // Thực hiện truy vấn check phòng 
+            if(result.length == 0){
+                var vqueryInsertNewRoom = "INSERT INTO `room`(`roomname`, `host`, `guest`, `count`) VALUES ('"+vRoomName+"','"+data+"','',1)"; // Tạo truy vấn thêm phòng mới
+
+                dbConnection.query(vqueryInsertNewRoom, function (err){ // Thực hiện truy vấn thêm phòng mới
+                    if(err) throw err
+                    console.log("Đã thêm phòng thành công");
+
+                    socket.join(vRoomName); // Vào phòng vRoomName
+
+                    var objectDataNewRoomServerSendClient = 
+                        {
+                            NewRoomName : vRoomName,
+                            isCheckChange : true
+                        }
+
+                    socket.emit("socketServerSendChangePageToBoard", objectDataNewRoomServerSendClient);
+                })
+            }else{
+                console.log("Đã trùng phòng");
+            }
+        })
+
+    });
+
+    //Chức năng lấy danh sách phòng từ CSDL
+    socket.on("socketClientSendRequestListRoom", function(Data){
+        if(Data){
+            console.log(Data)
+            var vqueryGetListRoom = "SELECT * FROM `room`"; //Tạo câu truy vấn để lấy dữ liệu từ CSDL
+
+            dbConnection.query(vqueryGetListRoom, function(err, result){ //Thực hiện câu truy vấn để lấy dữ liệu
+                // console.log(result); //Thực hiện kiểm tra xem có bao nhiêu phòng
+                
+                socket.emit("socketServerSendRequestListRoom", result); // Gửi lại dữ liệu phòng cho client
+            });
+
+        }
+    })
+
+
+    //Chức năng đánh cờ
     //Server nhận dữ liệu từ client khi người dùng di chuyển quân cờ
     socket.on('socketClientSendDataQuanCoToServer', function(data){
         console.log("dữ liệu nhận được: " +  data);
